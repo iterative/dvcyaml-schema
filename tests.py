012249f7e91349cfd03956aea5b2abb5ecce52c5
Saugat Pachhai (सौगात) <suagatchhetri@outlook.com>
2022-09-14 13:57:05 +0545

    add test example

--- tests.py
+++ tests.py
@@ -13,6 +13,8 @@ yaml = YAML(typ="safe")
 
 root = Path(__file__).resolve().parent
 examples_dir = root / "examples"
+valid_dir = examples_dir / "valid"
+invalid_dir = examples_dir / "invalid"
 
 
 @pytest.fixture
@@ -20,12 +22,20 @@ def schema():
     return json.loads((root / "schema.json").read_text(encoding="utf-8"))
 
 
-@pytest.mark.parametrize("example", (examples_dir / "valid").iterdir())
+def ids_gen(val: str) -> str:
+    if not isinstance(val, Path):
+        return str(val)
+    if val.name.endswith(".dvc.yaml"):
+        return val.name[:-9]
+    return val.name
+
+
+@pytest.mark.parametrize("example", valid_dir.iterdir(), ids=ids_gen)
 def test_valid_examples(schema, example):
     jsonschema.validate(yaml.load(example), schema=schema)
 
 
-@pytest.mark.parametrize("example", (examples_dir / "invalid").iterdir())
+@pytest.mark.parametrize("example", invalid_dir.iterdir(), ids=ids_gen)
 def test_invalid_examples(schema, example):
     with pytest.raises(jsonschema.ValidationError):
         jsonschema.validate(yaml.load(example), schema=schema)
